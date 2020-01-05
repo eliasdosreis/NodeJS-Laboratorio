fbc9deb36b5319f38639ba2739c31b4713404d70
Elias <elias_dos_reis@hotmail.com>
2020-01-05 12:35:42 -0300

    Fazendo Rotas com arquivo HTML

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express()
 const port = 3001
 
 // Rotas
-app.get('/Inicio',(req,res) => res.send('Pagina Inicial'))
-app.get('/Sobre',(req,res) =>  res.send('Sobre') )
-app.get('/Contato',(req,res) => res.send('Contato'))
+app.get('/Inicio',(req,res) => res.sendFile(__dirname + '/html/index.html'))
+app.get('/Sobre',(req,res) =>  res.sendFile(__dirname + '/html/sobre.html'))
+app.get('/Contato',(req,res) => res.sendFile(__dirname + '/html/contato.html'))
 
 // Adicionando porta
 app.listen(port,() => console.log(`Servidor foi iniciado na porta.: ${port}`))
